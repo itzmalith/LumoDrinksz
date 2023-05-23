d9e0e33a5f5fe14c563bb465057323ee8d83ee99
itzmalith <malith.20200144@iit.ac.lk>
2023-05-23 22:00:43 +0530

    Initial Commit

---
+++ LumoDrin/LumoDrinApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoDrinApp.swift
+//  LumoDrin
+//
+//  Created by Malith Lekamge on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct LumoDrinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
